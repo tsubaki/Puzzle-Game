2b60e7cd799537a0c65a5196728531313e4f6580
山村 達彦 <ruitomo.ex@gmail.com>
2014-03-01 21:31:14 +0900

    first commit

---
+++ Assets/BallController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallController : MonoBehaviour {
+
+	public enum BallColorType
+	{
+		Red = 0,
+		Yello = 1,
+		Blue = 2,
+		Green = 3,
+		White = 4,
+		Gray = 5
+	}
+
+	BallColorType m_color;
+
+	public BallColorType BallColor {
+		get{	return m_color; }
+		set{	ChangeColor(value); }
+	}
+
+	void ChangeColor(BallColorType color)
+	{
+		switch( color)
+		{
+		case BallColorType.Blue:	renderer.material.color = Color.blue; 	break;
+		case BallColorType.Red:		renderer.material.color = Color.red;	break;
+		case BallColorType.Yello:	renderer.material.color = Color.yellow;	break;
+		case BallColorType.Green:	renderer.material.color = Color.green;	break;
+		case BallColorType.White:	renderer.material.color = Color.white;	break;
+		case BallColorType.Gray:	renderer.material.color = Color.gray;	break;
+		}
+		m_color = color;
+	}
+
+	void Start()
+	{
+		ChangeRandomColor();
+	}
+
+	public void ChangeRandomColor()
+	{
+		BallColor = (BallColorType)Random.Range(0, 4);
+	}
+}
---
+++ Assets/TouchControll.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TouchControll : MonoBehaviour
+{
+
+	public Camera m_targetCamera;
+	public LineRenderer m_lineRenderer;
+
+	private BallController.BallColorType m_currentBallColor;
+	private List<BallController> m_ballList = new List<BallController> ();
+	private BallController m_currentTouchBall;
+
+	void Start()
+	{
+		m_targetCamera = Camera.main;
+	}
+
+	void Update ()
+	{
+		DrawLine();
+
+		if (Input.GetMouseButtonUp (0)) {
+			RemoveBalls ();
+		}
+
+		if (Input.GetMouseButton (0)) {
+			var position = m_targetCamera.ScreenToWorldPoint (Input.mousePosition);
+			var hit = Physics2D.OverlapCircle (position, 0.1f);
+
+			if (hit == null)
+				return;
+
+			var ball = hit.GetComponent<BallController> ();
+
+			if (Input.GetMouseButtonDown (0) && hit.CompareTag ("Ball")) {
+				m_currentBallColor = ball.BallColor;
+				m_currentTouchBall = ball;
+				m_ballList.Add (ball);
+			} else {
+				if (m_currentTouchBall == ball) {
+					return;
+				}
+
+				if (hit.CompareTag ("Ball") && !m_ballList.Exists ((item) => item == ball) && ball.BallColor == m_currentBallColor) {
+					m_ballList.Add (ball);
+					m_currentTouchBall = ball;
+				} else {
+					RemoveBalls ();
+				}
+			}
+		}
+	}
+
+	void DrawLine()
+	{	
+		m_lineRenderer.SetVertexCount (m_ballList.Count);
+		int i = 0;
+		foreach (var item in m_ballList) {
+			m_lineRenderer.SetPosition (i, item.transform.position + m_targetCamera.transform.forward * -4);
+			i++;
+		}
+	}
+
+	void RemoveBalls ()
+	{
+		if (m_ballList.Count > 2) {
+			foreach (var item in m_ballList) {
+				item.transform.position += Vector3.up * 20;
+				item.ChangeRandomColor ();
+			}
+		}
+		m_ballList.Clear ();
+
+		m_lineRenderer.SetVertexCount (0);
+	}
+}
